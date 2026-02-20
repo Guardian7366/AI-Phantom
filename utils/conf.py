c0ce296ce58a5be61b39b4deedf5c1cf770063b3
Adrian-G <AL02996024@tecmilenio.mx>
2026-02-20 17:05:43 -0600

    More comments

--- utils/conf.py
+++ utils/conf.py
@@ -1,13 +1,15 @@
 import os
 
 # ============================================================
-# CONFIGURACIÓN BASE
+# BASIC CONFIGURATION
 # ============================================================
 
+#Screen dimensions
 WINDOW_WIDTH = 960
 WINDOW_HEIGHT = 640
 FPS = 60
 
+#File paths set into variables to retrieve assets
 ASSETS_PATH = "assets"
 SOUNDS_PATH = os.path.join(ASSETS_PATH, "sounds")
 IMAGES_PATH = os.path.join(ASSETS_PATH, "images")
